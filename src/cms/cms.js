8f0ba004db34ac9f0916926cc558829c542ec5df
Victor Lawrence <VictorLawrenceCode@gmail.com>
2019-01-09 15:13:50 -0500

    Removed antd

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,5 @@
 import CMS from 'netlify-cms'
 
-import { TimePicker } from 'antd'
-
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
@@ -9,5 +7,3 @@ import ProductPagePreview from './preview-templates/ProductPagePreview'
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
-
-CMS.registerWidget('time', TimePicker)
